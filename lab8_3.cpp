ad9acf3679d8061ec7d08c195e115b4338445937
Thittawin <ptkk00177@gmail.com>
2024-12-13 13:38:25 +0700

    lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,57 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+#include<string> 
+using namespace std ;
+
+int main(){
+    int age,bounty;
+    double height;
+    string name ;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age<=25){
+        cout << "Enter your height: ";
+        cin >> height ;
+        if(height < 100){
+            name = "Chopper";
+        }
+        else{
+            if(height < 180){
+                name = "Usopp";
+
+            }
+            else{
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if(bounty > 1100000000){
+                    name = "Zoro";
+                }
+                else{
+                    name = "Sanji";
+
+                }
+            }
+        }
+    }
+    else{
+        if(age<=60 && age > 25){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty > 500000000){
+                name = "Jinbe";
+            }
+            else{
+                name = "Franky";
+            }
+        }
+        else{
+             name = "Brook";
+        }
+    }
+    cout << "Your character = " << name ;
+return 0;
+}
